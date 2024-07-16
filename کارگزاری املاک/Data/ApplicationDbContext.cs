c1cf5e566a400f5576e04ee5168b14669d3070af
ahmadreza2343 <argolmakany@gmail.com>
2024-07-16 17:39:45 +0430

    Add project files.

---
+++ کارگزاری املاک/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace کارگزاری_املاک.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
