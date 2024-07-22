b423743528363bee6ad323c2f861e07cc00979c7
ahmadreza2343 <argolmakany@gmail.com>
2024-07-22 19:22:09 +0430

    Login Register

--- کارگزاری املاک/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ کارگزاری املاک/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -71,7 +71,7 @@ namespace کارگزاری_املاک.Areas.Identity.Pages.Account
             }
 
             // If we got this far, something failed, redisplay form
-            return Page();
+            return RedirectToPage("/Index");
         }
     }
 }
