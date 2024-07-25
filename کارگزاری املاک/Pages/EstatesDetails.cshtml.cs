63ae4ae1d73d2d96a55efd1097da8f18586a3eb5
ahmadreza2343 <argolmakany@gmail.com>
2024-07-26 01:16:01 +0430

    Add to favorite Estates

--- کارگزاری املاک/Pages/EstatesDetails.cshtml.cs
+++ کارگزاری املاک/Pages/EstatesDetails.cshtml.cs
@@ -26,9 +26,9 @@ namespace کارگزاری_املاک.Pages
                 return NotFound();
             }
 
-           var Estates = await _context.estates
-                .Include(c=>c.CategoryModel)
-                .FirstOrDefaultAsync(e => e.Id == id);
+            var Estates = await _context.estates
+                 .Include(c => c.CategoryModel)
+                 .FirstOrDefaultAsync(e => e.Id == id);
 
             if (Estates is null)
             {
@@ -38,14 +38,51 @@ namespace کارگزاری_املاک.Pages
             EstatesVM = new()
             {
                 Estate = Estates,
-                suggestedproducts = _context.estates.Include(c=>c.CategoryModel)
-                    .Where(e=>e.CategoryModel.Title == Estates.CategoryModel.Title && e.Id != Estates.Id)
+                suggestedproducts = _context.estates.Include(c => c.CategoryModel)
+                    .Where(e => e.CategoryModel.Title == Estates.CategoryModel.Title && e.Id != Estates.Id)
                     .Take(4)
                     .ToList()
             };
 
             return Page();
 
+        }
+
+        public async Task<IActionResult> OnPostAddToFavorites(int id )
+        {
+            if (User is null || !User.Identity.IsAuthenticated)
+            {
+                return Redirect("/Identity/Account/Login?returnUrl=/EstatesDetails?Id=" + id);
+            }
+
+
+
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+
+            var estates = await _context.estates.FirstOrDefaultAsync(e => e.Id == id);
+            if (estates is null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
+
+            var checkIfredundant = await _context.favorites.FirstOrDefaultAsync(f => f.userId == user.Id && f.EstateId == id);
+
+            if (checkIfredundant is null)
+            {
+                await _context.AddAsync(new FavoriteModel()
+                {
+                    EstateId = id,
+                    userId = user.Id
+                });
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToPage("EstatesDetails", new { id });
         }
     }
 }
